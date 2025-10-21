4c5a53f0aaa68c53dba077e41fb4fb571211f64b
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-10-21 07:48:47 -0500

    first code

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./objects/Sortify.js";
+export * from "./objects/SimpleObj.js";
+export * from "./objects/IdGen.js";
+export * from "./objects/CsvWithFilters.js";
+export * from "./objects/CsvFormatter.js";
+export * from "./objects/MyTemplate.js";
+export * from "./objects/MyTuples.js";
